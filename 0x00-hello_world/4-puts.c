61ecc050e38b34eaa146273782579f25beaec245
luche3002 <amukhobukwame@gmail.com>
2024-06-20 23:44:36 +0300

    Prints output using puts command instead of printf() command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-	puts("Programming is like building a multilingual puzzle");
+/*This is the header*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
