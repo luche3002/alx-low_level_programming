4a5c178c2a5942a1aba4bc15f75b3b65d8b986d5
luche3002 <amukhobukwame@gmail.com>
2024-06-23 03:17:13 +0300

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,21 +9,18 @@ int main(void)
 
 {
 	int tens;
-	int ones;
 
 	for (tens = 0; tens < 10; tens++)
 	{
-		for (ones = 0; ones < 10; ones++)
-		{
-			putchar(tens + '0');
-			putchar(ones + '0');
+		putchar(tens + '0');
 
-			if (!(tens == 9 && ones == 9))
-			{
-				putchar(',' && ' ');
-			}
+		if (tens != 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
