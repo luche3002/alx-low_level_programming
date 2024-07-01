a725afa040a8b88b676d0e5d3838dcd8c571cfcf
luche3002 <amukhobukwame@gmail.com>
2024-07-02 02:30:42 +0300

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @l: length of string
+ * @c: values of string
+ *
+ * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	int l = 0;
+	char *strt = s;
+	char *end = s;
+	char c;
+
+	while (*end != '\0')
+	{
+		l++;
+		end++;
+	}
+	end--;
+
+	while (strt < end)
+	{
+		c = *strt;
+		*strt = *end;
+		*end = c;
+
+		strt++;
+		end--;
+	}
+}
