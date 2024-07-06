b63109e0573819e3075cb7e25593b71d7db16008
luche3002 <amukhobukwame@gmail.com>
2024-07-06 15:29:00 +0300

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,130 +1,20 @@
-#include <unistd.h>
-#include <stdio.h>
-#include "0-main.c"
+#include "main.h"
 
 /**
- * main - Entry point
+ * _isupper - checks for an uppercase letter
  *
- * Return: Always 0 (Success)
+ * Return: 0 or 1
  *
  */
-
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
-}
-
-int main()
-{
-	char testChar1 = 'A';
-	char testChar2 = 'a';
-	char testChar3 = 'Z';
-	char testChar4 = 'z';
-	char testChar5 = 'M';
-
-	if (_isupper(testChar1))
+	else
 	{
-		_putchar(testChar1);
-		_putchar(' ');
-		_putchar('i');
-		_putchar('s');
-		_putchar(' ');
-		_putchar('u');
-		_putchar('p');
-		_putchar('p');
-		_putchar('e');
-		_putchar('r');
-		_putchar('c');
-		_putchar('a');
-		_putchar('s');
-		_putchar('e');
-		_putchar('\n');
+		return (0);
 	}
-
-	if (_isupper(testChar2))
-	{
-		_putchar(testChar2);
-		_putchar(' ');
-		_putchar('i');
-		_putchar('s');
-		_putchar(' ');
-		_putchar('u');
-		_putchar('p');
-		_putchar('p');
-		_putchar('e');
-		_putchar('r');
-		_putchar('c');
-		_putchar('a');
-		_putchar('s');
-		_putchar('e');
-		_putchar('\n');
-	}
-
-	if (_isupper(testChar3))
-	{
-		_putchar(testChar3);
-		_putchar(' ');
-		_putchar('i');
-		_putchar('s');
-		_putchar(' ');
-		_putchar('u');
-		_putchar('p');
-		_putchar('p');
-		_putchar('e');
-		_putchar('r');
-		_putchar('c');
-		_putchar('a');
-		_putchar('s');
-		_putchar('e');
-		_putchar('\n');
-	}
-
-	if (_isupper(testChar4))
-	{
-		_putchar(testChar4);
-		_putchar(' ');
-		_putchar('i');
-		_putchar('s');
-		_putchar(' ');
-		_putchar('u');
-		_putchar('p');
-		_putchar('p');
-		_putchar('e');
-		_putchar('r');
-		_putchar('c');
-		_putchar('a');
-		_putchar('s');
-		_putchar('e');
-		_putchar('\n');
-	}
-
-	if (_isupper(testChar5))
-	{
-		_putchar(testChar5);
-		_putchar(' ');
-		_putchar('i');
-		_putchar('s');
-		_putchar(' ');
-		_putchar('u');
-		_putchar('p');
-		_putchar('p');
-		_putchar('e');
-		_putchar('r');
-		_putchar('c');
-		_putchar('a');
-		_putchar('s');
-		_putchar('e');
-		_putchar('\n');
-	}
-
-	return 0;
 }
