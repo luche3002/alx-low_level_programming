72f7fcb124350db9a37af53208e2e9fcc2a04d05
luche3002 <amukhobukwame@gmail.com>
2024-07-07 00:33:47 +0300

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square in the terminal
+ * @size: The size of the square
+ *
+ * Return: A square in the terminal
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+		_putchar ('\n');
+	}
+}
