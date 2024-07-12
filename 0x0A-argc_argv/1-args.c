013448a4698a7841109cfd355a62d64c4de13cfa
luche3002 <amukhobukwame@gmail.com>
2024-07-12 22:02:12 +0300

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into the program
+ * @argc: number of command line arguments
+ * @argv: array containing command line arguments
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	
+	printf("%d\n", argc - 1);
+
+	return(0);
+}
