f96e5531cc44968ec7cedc9d59bb97072f697540
luche3002 <amukhobukwame@gmail.com>
2024-07-12 23:51:14 +0300

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.c>
+
+/**
+ * main - prints all received arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(int argc, char *argv)
+{
+	int a;
+
+	for (a = 0; a < argc; a++;)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
