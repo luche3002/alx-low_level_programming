0ada5d7a0930cabcd1255ba44964a0e546f33f4f
luche3002 <amukhobukwame@gmail.com>
2024-07-18 18:19:48 +0300

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = malloc(nmemb * size);
+	
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
