a7f7d213122ea9c7462a83fe002431420e8ddaeb
luche3002 <amukhobukwame@gmail.com>
2024-10-13 18:27:57 +0300

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: The hash table to look into
+ * @key: The key to look for
+ *
+ * Return: The value associated with key, or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	/* Get the index for this key */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* Look for the key in the linked list at this index */
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
